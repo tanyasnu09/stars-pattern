fc9c26280d029aec43c17b0ea276a6f9f5be64f8
TANYA SHANKAR <89242225+tanyasnu09@users.noreply.github.com>
2022-07-16 11:38:35 +0530

    Add files via upload

---
+++ starpatt2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int i,j;
+    for (i=0;i<=5;i++)
+    {
+
+         for(j=6-i;j>1;j--)
+         {
+
+
+
+            printf("*");
+         }
+
+        printf("\n");
+    }
+}
